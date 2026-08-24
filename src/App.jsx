22929ecfdeb3e308fab3fa0ffd5f70e4a75bf953
Kourosh Nozari <kouroshnozari.pro@gmail.com>
2026-08-24 15:23:50 +0330

    fix: some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
 import { CoinDetail } from "./pages/CoinDetail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/coin/:id" element={<CoinDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
